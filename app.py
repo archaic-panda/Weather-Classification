b853cd318e43fc7c68c4cb800ddcb48deba859c1
Noku <tavongasaungweme11@outlook.com>
2024-06-28 16:08:10 +0200

    UI Design

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import pickle 
+import numpy as np
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+@app.route('/')
+def hello_world():
+    return render_template()
+
+@app.route('/predict', methods=['POST', 'GET'])
+def predict():
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
