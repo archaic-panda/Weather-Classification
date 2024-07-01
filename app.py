8bb7a54842be03a9de6e72fc8db3d3fe01488888
Noku <tavongasaungweme11@outlook.com>
2024-07-01 12:09:13 +0200

    testing web app

--- app.py
+++ app.py
@@ -3,16 +3,25 @@ import pickle
 import numpy as np
 app = Flask(__name__)
 
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('model_pl.pkl', 'rb'))
 @app.route('/')
 def hello_world():
-    return render_template()
+    return render_template("weather.html")
 
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
+    float_features = [float(x) for x in request.form.values()]
+    day_info = [np.array(float_features)]
+    prediction =  model.predict(day_info)
 
-
-
+    if prediction[0] == 0:
+        return render_template('weather.html', pred='Rainy')
+    elif prediction[0] == 1:
+        return render_template('weather.html', pred='Cloudy')
+    elif prediction[0] == 1:
+        return render_template('weather.html', pred='Sunny')
+    else:
+        return render_template('weather.html', pred='Snowy')
 
 
 if __name__ == '__main__':
